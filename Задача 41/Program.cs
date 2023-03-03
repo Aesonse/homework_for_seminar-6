4c240ceb46297ce6cc7d1437fc1a64c02a72c2c1
Alisa <aesonse@yandex.ru>
2023-03-04 02:38:52 +0300

    дз 6

---
+++ Задача 41/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Clear();
+int[] InputIntArray()
+{
+    Console.Write("Введите массив чисел через пробел: ");
+    string line = Console.ReadLine();
+    string[] nums = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] array = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        array[i] = int.Parse(nums[i]);
+    }
+    return array;
+}
+
+int CountPositiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] numbers = InputIntArray();
+Console.WriteLine($"Положительных чисел введено: {CountPositiveNumber(numbers)}");
---
+++ Задача 43/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+ y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+Console.Clear();
+double DataInput(string message)
+{
+    Console.Write(message + ": ");
+    double result = double.Parse(Console.ReadLine());
+    return result;
+}
+
+double[] CrossPointCoordinates(double k1, double b1, double k2, double b2)
+{
+    double[] coordinates = {(b1 - b2) / (k2 - k1), (b1 * k2 - b2 * k1) / (k2 - k1)};
+    return coordinates;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.WriteLine($"[{String.Join(", ", arr)}]");
+}
+
+double k1 = DataInput("Введите k1");
+double b1 = DataInput("Введите b1");
+double k2 = DataInput("Введите k2");
+double b2 = DataInput("Введите b2");
+
+if (k1 == k2)
+{
+    if (b1 == b2) Console.WriteLine("Данные линии совпадают");
+    else Console.WriteLine("Данные линии параллельны");
+}
+else PrintArray(CrossPointCoordinates(k1, b1, k2, b2));
